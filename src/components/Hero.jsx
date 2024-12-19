05b3ddff453933f4163a2efa3e80766990a7e70e
Dylan <dylanzfernandes@gmail.com>
2024-12-19 07:49:11 +0530

    fixes for final year

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,11 +7,10 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt='sumz_logo' className='w-28 object-contain' />
-
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/TidbitsJS/Summize", "_blank")
+            window.open("https://github.com/Dzenfern", "_blank")
           }
           className='black_btn'
         >
@@ -21,10 +20,10 @@ const Hero = () => {
 
       <h1 className='head_text'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='orange_gradient '>OpenAI GPT-4</span>
+        <span className='orange_gradient '>AI</span>
       </h1>
       <h2 className='desc'>
-        Simplify your reading with Summize, an open-source article summarizer
+        Simplify your reading with Summize, an article summarizer
         that transforms lengthy articles into clear and concise summaries
       </h2>
     </header>
